21193daa15687523f2434657795175c136dfe1b9
juankza <juankza@github.com>
2015-12-02 08:53:24 +0100

    Bank Services - BankCode UNIQUE (DB).

--- bank_services/src/com/fpmislata/daw2/database/V000__STRUCTURE.sql
+++ bank_services/src/com/fpmislata/daw2/database/V000__STRUCTURE.sql
@@ -6,7 +6,8 @@ CREATE TABLE IF NOT EXISTS `bankentity` (
   `creationDate` datetime DEFAULT NULL,
   `address` varchar(100) DEFAULT NULL,
   `ctc` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`bankEntityID`)
+  PRIMARY KEY (`bankEntityID`),
+  UNIQUE KEY `bankCode` (`bankCode`)
 ) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `user` (
--- bank_services/src/com/fpmislata/daw2/database/V001__DATA.sql
+++ bank_services/src/com/fpmislata/daw2/database/V001__DATA.sql
@@ -1,8 +1,8 @@
 
 INSERT IGNORE INTO `bankentity` (`bankEntityID`, `name`, `bankCode`, `creationDate`, `address`, `ctc`) VALUES
 	(3, 'prueba', 12345, '2015-09-24 13:55:46', 'prueba', 'prueba12345'),
-	(4, 'abc', 12345, '2015-10-01 00:00:00', 'abc', 'abc12345'),
-	(5, 'abc', 12345, '2015-10-28 00:00:00', 'abc12345', 'abc1234567890');
+	(4, 'abc', 67890, '2015-10-01 00:00:00', 'abc', 'abc12345'),
+	(5, 'abc', 13579, '2015-10-28 00:00:00', 'abc12345', 'abc1234567890');
 
 INSERT IGNORE INTO `user` (`userID`, `name`, `password`, `email`, `role`) VALUES
         (3, 'admin', 'admin', 'admin@domain.com', 'ADMIN'),
