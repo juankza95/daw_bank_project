1532c217d2472de14c7c74370a9e80e0d34257fc
Lliurex <Lliurex@172.16.205.123>
2015-12-09 13:17:24 +0100

    finiquitando para desplegar

--- bank/web/bankentity/bankentity-delete.js
+++ bank/web/bankentity/bankentity-delete.js
@@ -1,6 +1,6 @@
 BankEntityDeleteController.$inject = ['$scope','$routeParams','bankEntityService','$location'];
 function BankEntityDeleteController($scope,$routeParams,bankEntityService,$location){
-    response = bankEntityService.detail($routeParams.bankEntityID).success(function (data, status, headers, config) {
+    var response = bankEntityService.detail($routeParams.bankEntityID).success(function (data, status, headers, config) {
         $scope.bankEntity = data;
     }).error(function (data, status, headers, config) {
         alert("HTTP request failed. Status: " + status);
---
+++ bank/web/index.js
@@ -0,0 +1,16 @@
+IndexController.$inject = ['$scope', '$location', '$http'];
+function IndexController($scope, $location, $http) {
+    $scope.login = function () {
+        var response = $http({
+            method: 'GET',
+            url: '/bank_api/api/login'
+        });
+        response.success(function (data, status, headers, config) {
+            $location.path('/main.html');
+        }).error(function (data, status, headers, config) {
+            alert("Hubo un error al hacer login");
+        });
+    };
+
+}
+loginApp.controller("IndexController", IndexController);
