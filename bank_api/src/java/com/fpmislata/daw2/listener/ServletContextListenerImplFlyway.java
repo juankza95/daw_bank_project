623328a508614ce5da789922ba7d5e071faae747
vicenteasd <vicenteasd@gmail.com>
2015-12-15 08:44:09 +0100

    Hibernate port
    Pasando de JDBC a Hibernate.

---
+++ bank_api/src/java/com/fpmislata/daw2/listener/ServletContextListenerImplFlyway.java
@@ -0,0 +1,34 @@
+
+package com.fpmislata.daw2.listener;
+
+import com.fpmislata.daw2.database.DatabaseMigration;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+public class ServletContextListenerImplFlyway implements ServletContextListener {
+    @Autowired
+    DatabaseMigration dbmigration;
+    
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        System.out.println("\nSTARTING...\n");
+        
+        WebApplicationContext webApplicationContext = WebApplicationContextUtils.getRequiredWebApplicationContext(sce.getServletContext());
+        AutowireCapableBeanFactory autowireCapableBeanFactory = webApplicationContext.getAutowireCapableBeanFactory();
+        autowireCapableBeanFactory.autowireBean(this);
+        
+        dbmigration.migrateDB();
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        System.out.println("\nSTOPPING...\n");
+    }
+
+}
