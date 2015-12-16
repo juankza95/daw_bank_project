446f3168008a2d9e7526afdc1e773f8039cd8b7b
Lliurex <Lliurex@172.16.205.123>
2015-12-16 09:52:38 +0100

    a las chicas de verdad les gusta el pollo frito

--- bank_api/src/java/com/fpmislata/daw2/controller/UserRESTController.java
+++ bank_api/src/java/com/fpmislata/daw2/controller/UserRESTController.java
@@ -9,9 +9,11 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import static javax.servlet.http.HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -49,4 +51,32 @@ public class UserRESTController {
             Logger.getLogger(UserRESTController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+
+    @RequestMapping(method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
+    public void insert(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, @RequestBody String jsonRequest) {
+        try {
+            User user = jsonTransformer.toObject(jsonRequest, User.class);
+            User insertedUser = userService.insert(user);
+            if (insertedUser != null) {
+                httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+                httpServletResponse.setContentType("application/json; charset=UTF-8");
+                httpServletResponse.getWriter().println(jsonTransformer.toJSON(insertedUser));
+            } else {
+                httpServletResponse.setStatus(SC_INTERNAL_SERVER_ERROR);
+            }
+        } catch (BusinessException bex) {
+            try {
+                httpServletResponse.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                httpServletResponse.setContentType("application/json; charset=UTF-8");
+                httpServletResponse.getWriter().println(jsonTransformer.toJSON(bex.getBusinessMessages()));
+            } catch (IOException e) {
+                httpServletResponse.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+                Logger.getLogger(UserRESTController.class.getName()).log(Level.SEVERE, null, e);
+            }
+        } catch (IOException ex) {
+            httpServletResponse.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            Logger.getLogger(UserRESTController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
 }
